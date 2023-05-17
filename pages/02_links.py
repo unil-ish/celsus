689f248e683afacf9e08615c215499bea0c00588
Déesse <77760910+Call-Me-Deesse@users.noreply.github.com>
2023-05-17 15:48:28 +0200

    about, links, rules

---
+++ pages/02_links.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import os
+from pathlib import Path
+
+#La page qui contient les liens utiles
+st.set_page_config(
+    page_title= "Links",
+    page_icon= "🔗",
+    layout = "centered",
+)
+
+st. title("Useful Links")
+
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
+
+intro_markdown = read_markdown_file(r'md_files\links.md')
+st.markdown(intro_markdown, unsafe_allow_html=True)
